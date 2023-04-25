4342fb56b96923cbcc8f8f285e0eb3a96e7b83d8
Bodescu Stefan Rrares <100295422+Stefan1811@users.noreply.github.com>
2023-04-26 01:10:43 +0300

    Add files via upload

---
+++ Homework5/src/main/java/com/mycompany/homework5/InvalidDataException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.homework5;
+public class InvalidDataException extends Exception {
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
